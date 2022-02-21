3baddad2a686c90a77fb9a9acabbc531cb24030a
shryu8902 <shryu8902@naver.com>
2022-02-21 09:20:32 +0900

    data.py

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 import json
 #%%
 class LTS_dataprep():
-    def __init__(self, target, json_root='./Data/data_info.json', flag='train', scale=True, inverse=False, timeenc = True, freq='H'):
+    def __init__(self, target, json_root='./Data/data_info.json', flag='train', scale=True, inverse=False, time_feature = True, freq='H'):
         assert target in ['ECL', 'ELIA', 'KAGGLE', 'PANAMA','PRECON','UCI','UMASS','US10','USHOME']
         assert flag in ['train','val','test']
         type_map = {'train':0, 'val':1, 'test':2}
@@ -32,31 +32,39 @@ class LTS_dataprep():
 
         split_indx =[0, np.int(n_dates*0.6)*24, np.int(n_dates*0.8)*24, len(df_raw)]
 
+        df_train = df_raw.iloc[split_indx[0]:split_indx[1]]
+        df_target = df_raw.iloc[split_indx[set_type]:split_indx[set_type+1]] #self.set_type
+
         if self.scale:
             self.load_scaler = StandardScaler()
             self.add_scaler = StandardScaler()
-            df_train = df_raw.iloc[split_indx[0]:split_indx[1]]
-            df_target = df_raw.iloc[split_indx[set_type]:split_indx[set_type+1]] #self.set_type
-
             self.load_scaler.fit(df_train[meta["load_col"]].values.reshape(-1,1)) #self.meta
-            scaled_load = load_scaler.transform(df_target[meta["load_col"]].values.reshape(-1,1)) #self.meta, self.load_scaler
+
+            load = load_scaler.transform(df_target[meta["load_col"]].values.reshape(-1,1)) #self.meta, self.load_scaler
 
             if self.add_var :
                 self.add_scaler.fit(df_train[meta["add_cols"]].values) #self.meta
-                scaled_add_vars = add_scaler.transform(df_target[meta["add_cols"]].values) #self.meta, self.add_scaler
-
-            X = np.concatenate([scaled_load, scaled_add_vars],axis=1) if self.add_var else scaled_load
-
+                add_vars = add_scaler.transform(df_target[meta["add_cols"]].values) #self.meta, self.add_scaler
         else :
-            df_target = df_raw.iloc[split_indx[set_type]:split_indx[set_type+1]] #self.set_type
             load = df_target[meta["load_col"]].values.reshape(-1,1)) #self.meta, self.load_scaler
             if self.add_var:
                 add_vars = df_target[meta["add_cols"]].values
+    
+        if time_feature:
+            time_series = pd.Series(df_target.index)
+            hour = time_series.dt.hour
+            month = time_series.dt.month
+            weekday = time_series.dt.weekday #mon~sun 1~7
+            week = time_series.dt.week #i-th week 0~52
+            season = time_series.dt.month%12 // 3 + 1
+            day = time_series.dt.dayofyear # 0~365
+            time_features = np.concatenate([hour,month,weekday,week,season,day],axis=1)
+        
+            X = np.concatenate([load, time_features, add_vars],axis=1) if self.add_var else load
+        else:
             X = np.concatenate([load, add_vars],axis=1) if self.add_var else load
-
-        if self.timeenc:
             
-
+        self.X = X
 
 
 
